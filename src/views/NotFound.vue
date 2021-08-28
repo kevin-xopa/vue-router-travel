442beb761cba630a0459a81b4080b4cabdb5c196
Kevin Ochoa Xopa <kevinochoaxopa@gmail.com>
2021-08-27 23:43:20 -0500

    conclucion de curso vue router

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Not Founs</h1>
+    <p>Oops we couldn't find that page, Try going</p>
+    <router-link :to="{ name: 'Home' }">Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
